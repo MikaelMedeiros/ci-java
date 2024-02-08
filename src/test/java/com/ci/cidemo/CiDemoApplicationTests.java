7b1bc9b6c10b31c4e299072987f5180719cdc999
Mikael Medeiros <mikaeltruth@gmail.com>
2024-02-07 23:07:10 -0300

    first commit

---
+++ src/test/java/com/ci/cidemo/CiDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ci.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Teste feito.");
+	}
+
+}
